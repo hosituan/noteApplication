c4623b65da26f91baa0930cc667ea5e33a02da0d
LuciferBunny1998 <LuciferBunny1998>
2021-04-30 23:40:58 +0700

    Tung test 12345

--- app/src/main/java/com/example/noteapplication/activity/SecondFragment.java
+++ app/src/main/java/com/example/noteapplication/activity/SecondFragment.java
@@ -12,7 +12,7 @@ import androidx.navigation.fragment.NavHostFragment;
 import com.example.noteapplication.R;
 
 public class SecondFragment extends Fragment {
-
+    int a;
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
